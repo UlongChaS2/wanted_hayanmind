78611ccfb6361c9eaf9fb5e9176f19ccb09978d0
UlongChaS2 <jyujung2@gmail.com>
2021-08-14 23:38:56 +0900

    Feat: add constants.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from "react";
-import Comment from "./component/Comment/Comment";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import 'intersection-observer';
+import { LIMIT } from './utils/constants';
+import Comment from './component/Comment/Comment';
+import styled from 'styled-components';
 
 export default function App() {
-  const [data, setIsdata] = useState([]);
+  const [totalData, setTotalData] = useState([]);
   const [loading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [target, setTarget] = useState(null);
 
   // data fetch
   const getItems = () => {
     setIsLoading(true);
     fetch(
-      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`,
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=LIMIT`,
       {
-        method: "GET",
+        method: 'GET',
       }
     )
       .then((res) => res.json())
       .then((data) => {
-        setIsdata((prev) => prev.concat(data));
+        setTotalData((prev) => prev.concat(data));
         setIsLoading(false);
       });
   };
@@ -34,13 +37,13 @@ export default function App() {
     if (entry.isIntersecting) {
       // 이슈 3. 초기 마운트때 20개.
       if (!loading) {
+        console.log('entry :', entry);
+        console.log('entry.isIntersecting : ', entry.isIntersecting);
         // 이슈 2. 무한 page 증식
         setPage((prev) => prev + 1);
       }
     }
   };
-
-  const [target, setTarget] = useState(null);
 
   useEffect(() => {
     let observer;
@@ -54,11 +57,9 @@ export default function App() {
   return (
     <Container>
       <Wrap>
-        <Comment data={data} />
+        <Comment data={totalData} />
       </Wrap>
-      <div className="Loading" ref={setTarget}>
-        {loading && "Loading..."}
-      </div>
+      <Loding ref={setTarget}>{loading && 'Loading...'}</Loding>
     </Container>
   );
 }
@@ -73,4 +74,8 @@ const Container = styled.div`
 const Wrap = styled.div`
   width: 500px;
   margin-top: 26px;
+`;
+
+const Loding = styled.div`
+  font-size: 100px;
 `;
